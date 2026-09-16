153221d624f7098d8a1f6d6a8cad3f4bf2e38486
linux-rewritten-kernels <feddewestrahotmail@icloud.com>
2026-09-16 09:23:55 +0200

    Enhance package metadata in __init__.py
    Added a description and version to the package.

--- nexo_os_info/__init__.py
+++ nexo_os_info/__init__.py
@@ -1,5 +1,19 @@
 """
 Nexo OS Info
+A simple Python package for retrieving operating system information
+and GitHub release changelogs.
+"""
+
+from .os import get_os_name
+from .github import get_changelog
+
+__version__ = "0.0.1.2"
+
+__all__ = [
+    "get_os_name",
+    "get_changelog",
+]"""
+Nexo OS Info
 A simple Python package for retrieving operating system information.
 """
 
