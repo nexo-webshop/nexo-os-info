c5498e4119fba5a0fe141d02f4782c7867d22d63
linux-rewritten-kernels <feddewestrahotmail@icloud.com>
2026-09-16 09:57:46 +0200

    Add more system information functions to the module
    Expanded the module to include additional system information retrieval functions.

--- nexo_os_info/__init__.py
+++ nexo_os_info/__init__.py
@@ -1,15 +1,37 @@
 """
 Nexo OS Info
-A simple Python package for retrieving operating system information
-and GitHub release changelogs.
+A simple Python package for retrieving operating system and system information.
 """
 
-from .os import get_os_name
+from .os import (
+    get_os_name,
+    get_os_version,
+    get_os_architecture,
+    get_machine_name,
+    get_python_version,
+    get_processor,
+    get_hostname,
+    get_system_info,
+    is_windows,
+    is_linux,
+    is_macos,
+)
+
 from .github import get_changelog
 
 __version__ = "0.0.1.4"
 
 __all__ = [
     "get_os_name",
+    "get_os_version",
+    "get_os_architecture",
+    "get_machine_name",
+    "get_python_version",
+    "get_processor",
+    "get_hostname",
+    "get_system_info",
+    "is_windows",
+    "is_linux",
+    "is_macos",
     "get_changelog",
 ]
