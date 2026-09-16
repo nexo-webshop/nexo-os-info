6f554955fa7fcc06634c5238e212f5999fd076b0
linux-rewritten-kernels <feddewestrahotmail@icloud.com>
2026-09-16 09:17:12 +0200

    Update package description and version number

--- nexo_os_info/__init__.py
+++ nexo_os_info/__init__.py
@@ -1,7 +1,10 @@
 """
 Nexo OS Info
-A Python OS Info Retriever, made to be easy. available now.
+A simple Python package for retrieving operating system information.
 """
+
 from .os import get_os_name
 
-__version__ = "0.0.1"
+__version__ = "0.0.1.2"
+
+__all__ = ["get_os_name"]
